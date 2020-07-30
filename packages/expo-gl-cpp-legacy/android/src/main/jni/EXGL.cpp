b131ab507d95c1e57dde44c3046b0a55d34d0d6d
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2020-07-30 12:46:47 +0200

    switch old sdks to expo-gl-cpp-legacy

--- packages/expo-gl-cpp-legacy/android/src/main/jni/EXGL.cpp
+++ packages/expo-gl-cpp-legacy/android/src/main/jni/EXGL.cpp
@@ -14,7 +14,7 @@ extern "C" {
 #endif
 
 JNIEXPORT jint JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextCreate
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextCreate
 (JNIEnv *env, jclass clazz, jlong jsCtxPtr) {
   JSGlobalContextRef jsCtx = (JSGlobalContextRef) (intptr_t) jsCtxPtr;
   if (jsCtx) {
@@ -24,7 +24,7 @@ Java_expo_modules_gl_cpp_EXGL_EXGLContextCreate
 }
 
 JNIEXPORT jint JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextCreateV2
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextCreateV2
 (JNIEnv *env, jclass clazz, jlong jsCtxPtr) {
   // In react-native 0.59 jsCtxPtr is pointing to runtime object (in case of JSC it's JSCRuntime class)
   // implementing JSI interface. Real JSC context ref is extracted by offset from that object.
@@ -37,43 +37,43 @@ Java_expo_modules_gl_cpp_EXGL_EXGLContextCreateV2
 }
 
 JNIEXPORT void JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextDestroy
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextDestroy
 (JNIEnv *env, jclass clazz, jint exglCtxId) {
   UEXGLContextDestroy(exglCtxId);
 }
 
 JNIEXPORT void JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextFlush
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextFlush
 (JNIEnv *env, jclass clazz, jint exglCtxId) {
   UEXGLContextFlush(exglCtxId);
 }
 
 JNIEXPORT jint JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextCreateObject
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextCreateObject
 (JNIEnv *env, jclass clazz, jint exglCtxId) {
   return UEXGLContextCreateObject(exglCtxId);
 }
 
 JNIEXPORT void JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextDestroyObject
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextDestroyObject
 (JNIEnv *env, jclass clazz, jint exglCtxId, jint exglObjId) {
   UEXGLContextDestroyObject(exglCtxId, exglObjId);
 }
 
 JNIEXPORT void JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextMapObject
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextMapObject
 (JNIEnv *env, jclass clazz, jint exglCtxId, jint exglObjId, jint glObj) {
   UEXGLContextMapObject(exglCtxId, exglObjId, glObj);
 }
 
 JNIEXPORT jint JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextGetObject
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextGetObject
 (JNIEnv *env, jclass clazz, jint exglCtxId, jint exglObjId) {
   return UEXGLContextGetObject(exglCtxId, exglObjId);
 }
 
 JNIEXPORT void JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextSetFlushMethod
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextSetFlushMethod
 (JNIEnv *env, jclass clazz, jint exglCtxId, jobject glContext) {
   jclass GLContextClass = env->GetObjectClass(glContext);
   jobject glContextRef = env->NewGlobalRef(glContext);
@@ -86,13 +86,13 @@ Java_expo_modules_gl_cpp_EXGL_EXGLContextSetFlushMethod
 }
 
 JNIEXPORT bool JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextNeedsRedraw
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextNeedsRedraw
 (JNIEnv *env, jclass clazz, jint exglCtxId) {
   return UEXGLContextNeedsRedraw(exglCtxId);
 }
 
 JNIEXPORT void JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLContextDrawEnded
+Java_expo_modules_gl_cpp_legacy_EXGL_EXGLContextDrawEnded
 (JNIEnv *env, jclass clazz, jint exglCtxId) {
   UEXGLContextDrawEnded(exglCtxId);
 }
